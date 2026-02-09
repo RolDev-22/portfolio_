c8344daf5fc1847a2b10ceaa5b8a183ed0491ae2
RolDev-22 <rolandomuagui1629@gmail.com>
2026-02-08 19:35:43 -0600

    Ceacion de la estructura basica de components header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { lazy, Suspense } from "react";
+import Header from "@components/layout/header/Header";
+import Footer from "@components/layout/footer/Footer";
+
+const AboutSection = lazy(() => import("@features/aboutSection/About"));
+const ContactSection = lazy(() => import("@features/contactSection/Contact"));
+const HeroSection = lazy(() => import("@features/heroSection/Hero"));
+const ProjectSection = lazy(() => import("@features/projectsSection/Project"));
+const ServiceSection = lazy(() => import("@features/serviceSection/Service"));
+
 function App() {
   return (
-    <div className="App ">
-      <h1 className="titleStyles">Welcome to My React App!</h1>
-      <p className="textStyles">This is a simple React application.</p>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <HeroSection />
+          <AboutSection />
+          <ServiceSection />
+          <ProjectSection />
+          <ContactSection />
+        </Suspense>
+      </main>
+      <Footer />
+    </>
   );
 }
 
