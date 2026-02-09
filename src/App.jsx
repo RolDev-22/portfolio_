ed06618644a45957a229dbd1992ba6d34e3a1e00
RolDev-22 <rolandomuagui1629@gmail.com>
2026-02-08 19:03:19 -0600

    rEACOMODAMIENTO INICIAL DEL PROYECT, ESTRUCTURACION BÁSICA DEL MISMO

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
 function App() {
-  return <>hola</>;
+  return (
+    <div className="App ">
+      <h1 className="titleStyles">Welcome to My React App!</h1>
+      <p className="textStyles">This is a simple React application.</p>
+    </div>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@ui": path.resolve(__dirname, "./src/components/ui"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@data": path.resolve(__dirname, "./src/data"),
+    },
+  },
+});
