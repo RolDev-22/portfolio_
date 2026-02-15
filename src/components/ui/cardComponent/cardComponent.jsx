227e73630a0dc2c6258bd2bb60d08d6691cc224f
RolDev-22 <rolandomuagui1629@gmail.com>
2026-02-14 23:32:34 -0600

    se agregan los datos de los proyctos y se renderizan e el componente card, se realizan ajustes en este ultimo para un adecuado reutilizamiento

--- src/components/ui/cardComponent/cardComponent.jsx
+++ src/components/ui/cardComponent/cardComponent.jsx
@@ -2,27 +2,56 @@ import styles from "./cardComponent.module.css";
 
 const cardComp = ({
   title,
-  badge,
-  description,
-  url = "#Contact",
+  badge = "/imgproyect.jpg",
+  technologics = [],
+  description = "Description no disponible",
+  url = "#home",
+  urlPage = "",
   classId = "",
   textBtn = "Ver Más",
+  textBtn2 = "",
 }) => {
   return (
     <article
       className={`${styles.containerCard} ${classId ? styles[classId] : ""} zoomEffect`}>
-      <h2 className="subtitleStyles">{title}</h2>
-      <figure className={`${styles.fig} figureStyle`}>
-        <img className="imgStyle" src={badge} alt={title} title={title} />
-      </figure>
+      <section className={styles.containerTitle}>
+        <h2 className="subtitleStyles">{title}</h2>
+        <figure className={`${styles.fig} figureStyle`}>
+          <img className="imgStyle" src={badge} alt={title} title={title} />
+        </figure>
+        {technologics.length > 0 && (
+          <div className={styles.containerTechnologics}>
+            {technologics.map((tech, index) => (
+              <span key={index} className={styles.tech}>
+                {tech}
+              </span>
+            ))}
+          </div>
+        )}
+      </section>
+
       <h4 className="textStyles">{description}</h4>
-      <a
-        className="zoomEffect"
-        area-label="Ir al sitio del curso"
-        href={url ? url : ""}
-        rel="noopener noreferrer">
-        {textBtn}
-      </a>
+      <section className={styles.containerBtn}>
+        <a
+          className="zoomEffect"
+          area-label="Ir al sitio del curso"
+          href={url ? url : ""}
+          target={url ? "_blank" : "_self"}
+          rel="noopener noreferrer">
+          {textBtn}
+        </a>
+
+        {textBtn2 && (
+          <a
+            className="zoomEffect"
+            area-label="Ir al sitio del curso"
+            href={urlPage ? urlPage : ""}
+            target={urlPage ? "_blank" : "_self"}
+            rel="noopener noreferrer">
+            {textBtn2}
+          </a>
+        )}
+      </section>
     </article>
   );
 };
--- src/features/projectsSection/Project.jsx
+++ src/features/projectsSection/Project.jsx
@@ -1,7 +1,38 @@
+import styles from "./Project.module.css";
+import CardComp from "@/components/ui/cardComponent/cardComponent";
+import dataProjects from "@data/dataProject.json";
+
 export default function Project() {
+  const { projects } = dataProjects;
   return (
-    <section id="proyects" className="sectionStyle">
-      <h2 className="titleStyles">Projects</h2>
+    <section id="proyects" className={`${styles.customize} sectionStyle`}>
+      <section className={styles.containerTitle}>
+        <h2 className="titleStyles">Proyectos</h2>
+        <h3 className="subtitleStyles">
+          Transformando ideas en realidades digitales
+        </h3>
+      </section>
+      <section className={styles.containerProject}>
+        <section className={styles.container}>
+          {projects.length > 0 ? (
+            projects.map((item, index) => (
+              <CardComp
+                key={index}
+                title={item.title}
+                technologics={item.tecnologics}
+                description={item.description}
+                textBtn="Código"
+                textBtn2="Visitar"
+                url={item.urlGit}
+                urlPage={item.urlPage}
+                classId="proyect"
+              />
+            ))
+          ) : (
+            <p>No hay proyectos disponibles.</p>
+          )}
+        </section>
+      </section>
     </section>
   );
 }
