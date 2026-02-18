d0b9516044dbd27658687156be64f813c5199608
RolDev-22 <rolandomuagui1629@gmail.com>
2026-02-17 22:36:24 -0600

    actualizac[on de referencias

--- src/features/aboutSection/About.jsx
+++ src/features/aboutSection/About.jsx
@@ -1,7 +1,7 @@
 import styles from "./about.module.css";
 import ImgAvt from "/prfl-about.png";
-import IconComp from "@/components/ui/iconComponent/IconComponent.jsx";
-import CardComp from "@ui/cardComponent/cardComponent.jsx";
+import IconComp from "@ui/iconComponent/IconComponent";
+import CardComp from "@ui/cardComponent/CardComponent";
 import dataIcons from "@data/dataTecnologie.json";
 import dataCourse from "@data/dataCourse.json";
 
