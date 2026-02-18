4dcb93141665a4c29c5fdc059316e68629b9f477
RolDev-22 <rolandomuagui1629@gmail.com>
2026-02-17 22:47:18 -0600

    Actualizaci[on de referencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from "react";
 import Header from "@components/layout/header/Header";
 import Footer from "@components/layout/footer/Footer";
-import LoadingComp from "@ui/loadingComponent/loadingComponent.jsx";
+import LoadingComp from "@ui/loadingComponent/LoadingComponent";
 const AboutSection = lazy(() => import("@features/aboutSection/About"));
 const ContactSection = lazy(() => import("@features/contactSection/Contact"));
 const HeroSection = lazy(() => import("@features/heroSection/Hero"));
